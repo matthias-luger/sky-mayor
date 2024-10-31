658978e73472c36a582a085bd2c1ec21dd5e9bc7
Äkwav <ekwav@coflnet.com>
2024-10-31 14:19:50 +0100

    update generating

--- docs/generate.sh
+++ docs/generate.sh
@@ -1,5 +1,5 @@
 # generates the c# client
-VERSION=0.3.2
+VERSION=0.3.3
 NAME=Coflnet.Sky.Mayor.Client
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
@@ -13,7 +13,9 @@ sed -i 's/GIT_USER_ID/Coflnet/g' $path
 sed -i 's/GIT_REPO_ID/sky-mayor/g' $path
 sed -i 's/>OpenAPI/>Coflnet/g' $path
 sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
-sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
+sed -i '34i    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>' $path
 
 dotnet pack
 cp src/$NAME/bin/Release/$NAME.*.nupkg ..
+dotnet nuget push ../$NAME.$VERSION.nupkg --api-key $NUGET_API_KEY --source "nuget.org" --skip-duplicate
+
